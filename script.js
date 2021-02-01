a318255fc9bf2486338752b5be6bdd0066f69301
issaheim <75672568+issaheim@users.noreply.github.com>
2021-02-01 16:02:05 +0100

    created a new div for the numbers and styled the site

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 const rootDiv = document.getElementById("root");
 const clockDiv = document.createElement("div");
+const nrDiv = document.createElement("div")
 const h1 = document.createElement("h1");
 const h2 = document.createElement("h2");
 
 rootDiv.appendChild(h1);
 rootDiv.appendChild(clockDiv);
 
-clockDiv.id = clockDiv
+clockDiv.appendChild(nrDiv);
+
+clockDiv.id = "clockDiv";
+nrDiv.id = "nrDiv";
 
 function time(){
   let date = new Date();
   let h = date.getHours();
   let m = date.getMinutes();
   let s = date.getSeconds();
-
+  
   h = ("0" + h).slice(-2);
   m = ("0" + m).slice(-2);
   s = ("0" + s).slice(-2);
 
   let showTime = h + " : " + m + " : " + s;
   
-  clockDiv.innerText = showTime; 
-  clockDiv.textContent = showTime;
+  nrDiv.innerText = showTime; 
+  nrDiv.textContent = showTime;
 }
 
 setInterval(time, 1000);
